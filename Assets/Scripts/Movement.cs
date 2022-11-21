3afe96434a9afe6b7f66eb22b461a28b51af482a
Cosinus215 <oliwka_pawlowska@wp.pl>
2022-11-21 01:03:47 +0100

    Add player rotation and direction of looking

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,7 +6,18 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     private CharacterController characterController;
-    public Vector2 movementVector;
+    private Vector2 movementVector;
+    [SerializeField] private float deadZone = 0.25f;
+    [SerializeField] private GameObject directionPoint;
+    [SerializeField] private GameObject Octi;
+
+    private Quaternion rotationLeft    = new Quaternion(0, 0, 0, 0);
+    private Quaternion rotationRight   = new Quaternion(0, 180, 0, 0);
+
+    private Vector3 directionUp        = new Vector3(0, 0, 1.5f);
+    private Vector3 directionDown      = new Vector3(0, 0, -1.5f);
+    private Vector3 directionLeft      = new Vector3(-1.5f, 0, 0);
+    private Vector3 directionRight     = new Vector3(1.5f, 0, 0);
 
     void Start()
     {
@@ -16,13 +27,48 @@ public class Movement : MonoBehaviour
     public void PlayerMovement(InputAction.CallbackContext context)
     {
         movementVector = context.ReadValue<Vector2>();
+        Player_Rotation();
+        Player_LookDirection();
     }
 
     private void Update()
     {
-        if (movementVector != new Vector2(0, 0)) 
+        if (movementVector.magnitude > deadZone)
         {
             characterController.Move(new Vector3(movementVector.x, 0, movementVector.y) * Time.deltaTime * 10);
         }
     }
+
+    private void Player_Rotation()
+    {
+        if (movementVector.x > deadZone)
+        {
+            Octi.transform.localRotation = rotationRight;
+        }
+        else if (movementVector.x < -deadZone)
+        {
+            Octi.transform.localRotation = rotationLeft;
+        }
+    }
+
+    private void Player_LookDirection()
+    {
+        if (movementVector.x > deadZone)
+        {
+            directionPoint.transform.localPosition = directionRight;
+        }
+        else if (movementVector.x < -deadZone)
+        {
+            directionPoint.transform.localPosition = directionLeft;
+        }
+        else if (movementVector.y > deadZone)
+        {
+            directionPoint.transform.localPosition = directionUp;
+        }
+        else if (movementVector.y < -deadZone)
+        {
+            directionPoint.transform.localPosition = directionDown;
+        }
+    }
+
 }
