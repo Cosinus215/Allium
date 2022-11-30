f91feb6926d245f01a66f566e71b7e0c665add7f
Miłosz Klim <71337890+DAXPL@users.noreply.github.com>
2022-11-30 16:49:31 +0100

    still bad

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,70 +5,85 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("Overall")]
     public const int SECONDS_PER_TICK = 1;
     private WaitForSeconds tickTime = new WaitForSeconds(SECONDS_PER_TICK);
     [SerializeField] private GameEvent tickEvent;
 
-    public int dayTime = 0;
-    public int ticksPerDay  = 24;
-    public int sunriseTick = 6;
-    public int sunsetTick = 18;
-
-    public float currentSunAngle = 0;
-    public float sunriseSunAngle = 90;
-    public float sunsetSunAngle = 180;
+    [Header("Time")]
+    [SerializeField] private int dayTime = 0;
+    private int ticksPerDay  = 24;
+    private int sunriseTick = 6;
+    private int sunsetTick = 18;
+    private uint gameTick = 0;
+    [Space]
+    [SerializeField] private float currentSunAngle = 0;
+    [SerializeField] private float sunriseSunAngle = 90;
+    [SerializeField] private float sunsetSunAngle = 180;
+    [Space]
+    [SerializeField] private Transform sun;
     public enum Weather { Sunny, Cloudy, Rainy };
+    [Header("Weather")]
     public Weather currentWeather;
 
     private void Start()
     {
-        //Uruchamiamy petle ktora bedzie wewnetrznym zegarem naszej gry
+        //Internal game timer
         StartCoroutine(GameTick());
     }
-    //Petla uruchamia event so
-    //potem mozna dodac wiecej logiki
-    //Kurtyny mozesz traktowac (tylko do pewnego stopnia i uproszczone) jako watki
-    //Mozemy zrzucac tutaj akcje ktore dziac sie beda przez jakis czas
-    //Lub jakies ciezkie i wolne obliczenia, nie blokujac watku gry (nie ma zacinek)
+
     private IEnumerator GameTick()
     {
-        for (;;)//niech ci bedzie ze for ;)
+        for (;;)
         {
+            ++gameTick;
             tickEvent.Raise();
-            dayTime++;
-            if(dayTime > ticksPerDay)
-            {
-                dayTime = 0;
-            }
-            if (dayTime>=sunriseTick && dayTime<=sunsetTick)
-            {
-                //dzieñ
-                float sunPercentage = (float)((float)dayTime - (float)sunriseTick) / (float)((float)sunsetTick - (float)sunriseTick);
-                currentSunAngle = Mathf.Lerp(sunriseSunAngle,sunsetSunAngle, sunPercentage);
-                Debug.Log($"Day: {sunPercentage}");
-            }
-            else
-            {
-                //noc
-                /*
-                float nightPercentage = 0;
-                if (dayTime >= 0)
-                {
-                    nightPercentage = dayTime + ((float)ticksPerDay - sunsetTick) / (float)ticksPerDay - (sunsetTick + sunriseTick);
-                }
-                else
-                {
-                    nightPercentage = (float)ticksPerDay-dayTime / (float)ticksPerDay - (sunsetTick + sunriseTick);
-                }
-
-                currentSunAngle = Mathf.Lerp(sunsetSunAngle, sunriseSunAngle, nightPercentage);
-                Debug.Log($"Night: {nightPercentage}");
-                */
-            }
+            HandleDayCycle();
             yield return tickTime;
         }
     }
+    private void HandleDayCycle()
+    {
+        dayTime++;
+        if (dayTime > ticksPerDay)
+        {
+            dayTime = 0;
+        }
+        if (dayTime >= sunriseTick && dayTime <= sunsetTick)
+        {
+            //Day
+            float sunPercentage = (float)((float)dayTime - (float)sunriseTick) / (float)((float)sunsetTick - (float)sunriseTick);
+            currentSunAngle = Mathf.Lerp(sunriseSunAngle, sunsetSunAngle, sunPercentage);
+            //Debug.Log($"Day: {sunPercentage*100}%");
+        }
+        else
+        {
+            //Night
+            float nightLength = ticksPerDay - sunsetTick + sunriseTick;
+            float nightTick = 0;
+            if (dayTime < sunriseTick)
+            {
+                nightTick = (ticksPerDay - sunsetTick) + dayTime + 1;
+            }
+            else
+            {
+                nightTick = Mathf.Abs(dayTime - sunsetTick);
+            }
+            float nightPercentage = nightTick / nightLength;
+            currentSunAngle = Mathf.Lerp(sunsetSunAngle, sunriseSunAngle, nightPercentage);
+            //Debug.Log($"Night: {nightPercentage*100}% {nightTick} z {nightLength}");
+        }
 
+        if (sun)
+        {
+            sun.rotation = new Quaternion(currentSunAngle, 0, 0, 0);
+        }
+        
+    }
+    private void ChangeWeather()
+    {
+        
+    }
     public Weather GetCurrentWeather()
     {
         return currentWeather;
